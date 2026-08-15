6427b1df8c9cfeb3df3d29715dd2da453550c56c
yukt4 <j.unmole@campus.tu-berlin.de>
2026-08-15 22:58:07 +0200

    Add Spring Boot scaffolding from Spring Initializr

---
+++ backend/src/main/java/com/example/interlinked/InterlinkedApplication.java
@@ -0,0 +1,13 @@
+package com.example.interlinked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterlinkedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterlinkedApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/interlinked/InterlinkedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interlinked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterlinkedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
